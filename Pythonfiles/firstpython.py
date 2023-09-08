39bc3184cf5fcd7502b0c1b262df5f6c1915428c
Akshay-Sreenivasan <135939039+Akshay-Sreenivasan@users.noreply.github.com>
2023-09-08 09:00:03 +0530

    python

---
+++ Pythonfiles/firstpython.py
@@ -0,0 +1,42 @@
+# 07/09/2023-----------------------------
+# a=5
+# b=12
+# # a=a+b
+# # b=a-b
+# # a=a-b
+# a,b=b,a
+# print(a)
+# print(b)
+
+# 08/09/2023-----------------------------
+# 1st pgm
+# a=int(input("Enter the value"))
+# b=int(input("ENter the value"))
+# x=((a**2)+(b**2)+(2*a*b))
+# print(x)
+
+# 2nd pgm---------------------------------
+# a=int(input("Enter the value"))
+# b=int(input("ENter the value"))
+# c=int(input("ENter the value"))
+# x=int(input("ENter the value"))
+# y=((a*x**2)+(b*x)+(c))
+# print(y)
+
+# 3rd pgmm--------------------------------
+# list1=[1,5,2,6,4,8,4,10]
+# list1[3]=13
+# print(list1)
+# ----------------------------------------
+# t=(1,2,3,4,5,"hello")
+# y=list(t)
+# y[5]="OMG"
+# t=tuple(y)
+# print(t)
+# ----------------------------------------
+# s={1,1,2,3,4,5,"orange"}
+# y=list(s)
+# y[4]="OMG"
+# s=set(y)
+# print(s)
+# ----------------------------------------
