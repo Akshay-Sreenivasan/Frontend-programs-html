9d1519512e53633a423f9419529176f99ad07334
Akshay-Sreenivasan <135939039+Akshay-Sreenivasan@users.noreply.github.com>
2023-09-16 09:00:36 +0530

    python

---
+++ Pythonfiles/liststrinddcount.py
@@ -0,0 +1,12 @@
+lit=int(input("Enter the limit "))
+i=1
+list=[]
+count=[]
+while (i>=0 and i<=lit):
+    a=input("Enter the strings ")
+    list.append(a)
+    b=len(a)
+    count.append(b)
+    i=i+1
+print(list)
+print(count)
