6c594b8289ff7b9a5b0b63c6bb95e94b49453845
Akshay-Sreenivasan <135939039+Akshay-Sreenivasan@users.noreply.github.com>
2023-09-23 08:59:21 +0530

    Delete Pythonfiles directory

--- Pythonfiles/pyramid pattern.py
+++
@@ -1,6 +0,0 @@
-a=int(input("Enter the limit"))
-for i in range (0,a):
-    for j in range(0,i+1):
-        print("*",end=" ")
-    print(" ")
-
--- Pythonfiles/pyramidreverserigth.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Enter the Limit"))
-for i in range(a):
-    for j in range(i):
-        print(" ", end=" ")    
-    for j in range(a-i):
-        print("*", end=" ")
-    print()
